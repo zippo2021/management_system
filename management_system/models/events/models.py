2a9bde6f154c3e8b44cea0e677952d3681274218
UraharaLL <UraharaLL@yandex.ru>
2014-10-17 22:07:11 +0400

    event models were created

--- management_system/models/events/models.py
+++ management_system/models/events/models.py
@@ -1,4 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class AbstractEvent(models.Model):
+class Event(models.Model):
+    name = models.CharField(verbose_name = 'Название', max_length = 100)
+    comment = models.CharFielf(verbose_name = 'Комментарий', max_length = 1000,\                                    blank = True)
+    place = models.CharField(verbose_name = 'Место проведения', max_length = 100)
+    opened = models.DateField(verbose_name = 'Создано')
+    closed = models.DateField(verbose_name = 'Закрыто')
+    is_private = models.BooleanField()
+    is_journey = models.BooleanField()
+    is_payed = models.BooleanField()
 
+'''
+price and participants applied for this price
+'''
+class PriceGroup(models.Model):
+    event = models.ForeignKey(Event)
+    price = models.IntegerField(verbose_name = 'Стоимость')
+    users = models.ManyToManyField(User)
+    
+'''
+Comments about user according to this event
+'''
+class PersonalComment(models.Model):
+    event = models.ForeignKey(Event)
+    user = models.ForeignKey(User)
+    text = models.CharField(verbose_name = 'Комментарий', max_length = 1000)
+
+'''
+if event is a journey
+'''
+class JourneyData(models.Model):
+    ticket_price = models.IntegerField(verbose_name = 'Стоимость билетов')
+    departure_time = models.DateTimeField(verbose_name = 'Время и дата отправления')
+    departure_place = models.CharField(verbose_name = 'Место отправления', max_length = 100)
+
+'''
+if event is private
+'''
+class Requests(models.Model):
+    event = models.ForeignKey(Event)
+    users = models.ManyToManyField(User)
