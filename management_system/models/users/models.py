98c91902b60fb23633a8f61d7921e351fda6a151
Tony <zippo2021@gmail.com>
2014-10-19 23:02:42 +0400

    User model changes

--- management_system/models/users/models.py
+++ management_system/models/users/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserData(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField(verbose_name='Аватар',  upload_to='images/profile_pics',blank=True,null=True)
+    first_name = models.CharField(verbose_name='Имя',  max_length=255)
+    middle_name = models.CharField(verbose_name='Отчество',  max_length=255,blank=True, null=True)
+    last_name = models.CharField(verbose_name='Фамилия',  max_length=255)
+    phone  = models.CharField(verbose_name='Телефон',max_length=15,blank=True, null=True)
 
 class EventWorker(models.Model):
 
