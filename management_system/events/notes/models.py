c55e0d4baeb158a515e2186418a25f6f5b9a222c
alex <uraharaLL@yandex.ru>
2014-10-29 16:58:11 +0400

    minory changes

--- management_system/events/notes/models.py
+++ management_system/events/notes/models.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-from dashboard.teacher.models import Teacher
-from dashboard.regular.models import RegularUser
 # Create your models here.
 class Note(models.Model):
-	author = models.ForeignKey(Teacher)
-	receiver = models.ForeignKey(RegularUser)
+	author = models.ForeignKey('teacher.Teacher')
+	receiver = models.ForeignKey('regular.RegularUser')
 	text = models.CharField(verbose_name = 'Запись', max_length = 1000)
 
