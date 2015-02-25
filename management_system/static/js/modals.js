3bd02a07996c8655b82c0878ad58237813866e0b
Tony <zippo2021@gmail.com>
2015-02-25 21:24:34 +0400

    Wizard modality ready

--- management_system/static/js/modals.js
+++ management_system/static/js/modals.js
@@ -22,13 +22,14 @@ success : function(text)
                     url: post_url,
                     data: msg,
                     success: function(data) {
-                        modal.hide();
-                        modal.destroy();
+                        
                         if (data == "success"){
+                            modal.hide();
+                            modal.destroy();
                             OkMessageAutoClose("Данные сохранены.",2,true);
                         }
                         else{
-                            ErrorMessage("Error");
+                            modal.getContentElement().replaceWith("<div class='modal-body'>"+data+"</div>")
                         }
                     },
                     error: function(xhr, str){
@@ -51,59 +52,4 @@ success : function(text)
 });
 }
 
-function WizardModalToggle(get_url,post_url,t_id,t_title,step)
-{
-var content = '';
-$.ajax({ type: "GET", 
-url: get_url, 
-async: false,
-success : function(text)
-{
-    content = text;
-    var modal = new Modal();
-    modal.setTitle(t_title);
-    modal.getContentElement().append(content);
-    modal.setButtons([
-    {
-            label:'Сохранить',
-            callback:function(){
-                var msg = modal.getContentElement().find(t_id).serialize();
-                $.ajax({
-                    type: 'POST',
-                    url: post_url,
-                    data: msg,
-                    success: function(data) {
-                        if (data == "success"){
-                            modal.hide();
-                            modal.destroy();
-                            OkMessageAutoClose("Данные сохранены.",2,true);
-                        }
-                        else{
-                            var next_step = modal.getElementById("next_step").value;
-                            modal.hide();
-                            modal.destroy();
 
-                            alert(next_step);
-                            WizardModalToggle(get_url,post_url,t_id,t_title,next_step);                      
-                        }
-                        
-                    },  
-                    error: function(xhr, str){
-                        modal.hide();
-                        modal.destroy();
-                        ErrorMessage("Жопа");
-                    }
-                });
-        }
-    },
-    {
-        label:"Закрыть",
-        callback:function(){
-            modal.hide();
-            modal.destroy();
-        }
-    }]);
-    modal.show();
-}
-});
-}
