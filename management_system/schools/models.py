6256b0df2593c4eece4ab708e3482070ff4c83b5
alex <uraharaLL@yandex.ru>
2015-02-26 11:16:48 +0400

    fckn panel in the fckn middle

--- management_system/schools/models.py
+++ management_system/schools/models.py
@@ -4,7 +4,9 @@ from django.db import models
 # Create your models here.
 
 class School(models.Model):
-	name = models.CharField(verbose_name = 'Название', max_length = 100)
-	country = models.CharField(verbose_name = 'Страна', max_length = 30)
-	city = models.CharField(verbose_name = 'Город', max_length = 30)
-	approved = models.BooleanField(default = False)
+    def __unicode__(self):
+        return self.name+' '+self.city
+    name = models.CharField(verbose_name = 'Название', max_length = 100)
+    country = models.CharField(verbose_name = 'Страна', max_length = 30)
+    city = models.CharField(verbose_name = 'Город', max_length = 30)
+    approved = models.BooleanField(default = False)
